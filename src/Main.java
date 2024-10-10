799208409efb4a3006fafd89a2b741fb7a015fcf
MateiStefanica0 <matei.stefanica@gmail.com>
2024-10-10 12:46:15 +0300

    bsknfvow

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello, Matei Stefanica!");
-
+        System.out.println("jbydhcvsdv");
 
     }
 }
